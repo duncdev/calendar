2f1e44094d264a9c13b0dfa3c032d6cb640347f0
duncdev <duncdev@gmail.com>
2021-06-20 15:27:07 +0100

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import store from "./state/store";
 import App from "./App";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById('root')
+  <App />, document.getElementById('root')
 )
